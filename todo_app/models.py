4e6cdeb5d21938abcad60d657d884c3495d3dd7e
ACER\Stephen <steve@ocallaghan.me.uk>
2022-05-13 19:55:22 +0100

    Making file PEP compliant

--- todo_app/models.py
+++ todo_app/models.py
@@ -2,34 +2,26 @@
 
 from django.utils import timezone
 
-
 from django.db import models
 
 from django.urls import reverse
 
 
 def one_week_hence():
-
     return timezone.now() + timezone.timedelta(days=7)
 
 
 class ToDoList(models.Model):
-
     title = models.CharField(max_length=100, unique=True)
 
-
     def get_absolute_url(self):
-
         return reverse("list", args=[self.id])
 
-
     def __str__(self):
-
         return self.title
 
 
 class ToDoItem(models.Model):
-
     title = models.CharField(max_length=100)
 
     description = models.TextField(null=True, blank=True)
@@ -40,21 +32,15 @@ class ToDoItem(models.Model):
 
     todo_list = models.ForeignKey(ToDoList, on_delete=models.CASCADE)
 
-
     def get_absolute_url(self):
-
         return reverse(
 
             "item-update", args=[str(self.todo_list.id), str(self.id)]
 
         )
 
-
     def __str__(self):
-
         return f"{self.title}: due {self.due_date}"
 
-
     class Meta:
-
         ordering = ["due_date"]
