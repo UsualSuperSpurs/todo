41f5ae22cd6b156ae95c6bd83815c0565ab56858
ACER\Stephen <steve@ocallaghan.me.uk>
2022-05-24 20:14:11 +0100

    Simple passwords

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -86,6 +86,7 @@ DATABASES = {
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
+    """
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
@@ -97,7 +98,8 @@ AUTH_PASSWORD_VALIDATORS = [
     },
     {
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+    }, 
+    """
 ]
 
 
